953a2580f144e5cd9a3e7d4fc4c117144222712e
yusefellban <ellbanyousef@gmail.com>
2026-03-08 23:04:52 +0200

    Add color pallet and some themes

--- app/src/main/java/iti/yousef/skymood/ui/theme/Color.kt
+++ app/src/main/java/iti/yousef/skymood/ui/theme/Color.kt
@@ -2,10 +2,33 @@ package iti.yousef.skymood.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// --- Primary Sky Blues ---
+val SkyBlue = Color(0xFF42A5F5)
+val SkyBlueDark = Color(0xFF1565C0)
+val SkyBlueLight = Color(0xFF90CAF9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// --- Night / Dark Theme ---
+val NightDark = Color(0xFF0D1B2A)
+val NightMedium = Color(0xFF1B2838)
+val NightLight = Color(0xFF1A237E)
+
+// --- Sunset / Warm Accents ---
+val SunsetOrange = Color(0xFFFF7043)
+val SunsetGold = Color(0xFFFFF176)
+val SunsetPink = Color(0xFFEF5350)
+
+// --- Storm / Neutral ---
+val StormGray = Color(0xFF546E7A)
+val StormGrayLight = Color(0xFF78909C)
+val CloudWhite = Color(0xFFECEFF1)
+
+// --- Surface & Background ---
+val SurfaceDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFF5F5F5)
+val CardDark = Color(0xFF1E1E1E)
+val CardLight = Color(0xFFFFFFFF)
+
+// --- Text ---
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xB3FFFFFF)   // 70% white
+val TextOnLight = Color(0xFF212121)
